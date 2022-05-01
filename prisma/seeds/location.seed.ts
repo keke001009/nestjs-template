5620a998bfccb9f3042373f119b4b0f799589305
redtree <keke001009@gmail.com>
2022-05-01 21:26:34 +0900

    더미데이터 추가

--- prisma/seeds/location.seed.ts
+++ prisma/seeds/location.seed.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
@@ -11,18 +12,17 @@ export default async function main() {
       },
     })
   }
-  return await Promise.all([
-    upsert({
-        name: '공간1',
-        address: '공간1주소',
-        lat: 123.123,
-        lng: 123.123,
-    }),
-    upsert({
-        name: '공간2',
-        address: '공간2주소',
-        lat: 123.123,
-        lng: 123.123,
-    })
-  ])
+  const config = {
+    headers: {
+      "Referer": "https://map.kakao.com/",
+    },
+  };
+  const { data } = await axios.get('https://search.map.kakao.com/mapsearch/theme.daum?output=json&category=Y&theme_id=c86&cpage=1&rect=504815%2C1111925%2C506455%2C1113262', config);
+  console.log(data);
+  return await Promise.all(data?.place?.map((item: any) => upsert({
+    lng: item.lon,
+    lat: item.lat,
+    address: item.address,
+    name: item.name,
+  })))
 }
